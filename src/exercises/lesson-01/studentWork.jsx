85accba4411ddd73bf4355a706bd810342f29060
Heather813903 <heathersmith9378@gmail.com>
2026-07-05 18:12:11 -0400

    Complete lesson 01 exercises

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,25 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Heather';
+  const age = 48;
+  const hobbies = ['Going to the beach', 'Boxing', 'Traveling'];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+
+      <p>
+        Hi! My name is {name}, and I am {age} years old.
+      </p>
+
+      <h2>My Hobbies</h2>
+
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
